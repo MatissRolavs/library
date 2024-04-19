5fb2ddb3ded7e42316041a6b5f600a1aca44c658
Matiss Rolavs <matissrolavs2005@gmail.com>
2024-04-19 10:21:23 +0300

    edit

--- views/book-create.view.php
+++ views/book-create.view.php
@@ -4,28 +4,32 @@
 <?php
     require "components/navbar.php";
 ?>
-<div class="page-title">
-    <h1>Add more books</h1>
-</div>
-<form method="POST">
-    <label>
-        Title:
-        <input name="title"/>
-    </label>
-    <label>
-        Author:
-        <input name="author"/>
-    </label>
-    <label>
-        Published date:
-        <input name="published"/>
-    </label>
-    <label>
-        Available:
-        <input name="available"/>
-    </label>
-    <button>Save</button>
-</form>
+<article>
+    <div class="edit-box">
+        <div class="bookhub-box">
+            <h1>Add more books</h1>
+        </div>
+        <form method="POST">
+            <label>
+                Title:
+                <input  class="edit-input" name="title"/>
+            </label>
+            <label>
+                Author:
+                <input class="edit-input" name="author"/>
+            </label>
+            <label>
+                Published date:
+                <input class="edit-input"name="published"/>
+            </label>
+            <label>
+                Available:
+                <input class="edit-input"name="available"/>
+            </label>
+            <button class="book-edit-button">Save</button>
+        </form>
+    </div>
+</article>
 <?php
     require "components/footer.php";
 ?>
--- views/book-edit.view.php
+++ views/book-edit.view.php
@@ -4,31 +4,33 @@
 <?php
     require "views/components/navbar.php";
 ?>
-<div class="page-title">
-    <h1>Edit <?= $book["title"]?></h1>
-</div>
-
-<form method="POST"> 
-    <label>
-        Title:
-        <input name="title" value="<?= $_POST["title"] ?? $book['title'] ?>"/>
-    </label>
-    <label>
-        Author:
-        <input name="author" value="<?= $_POST["author"] ?? $book["author"] ?>"/>
-    </label>
-    <label>
-        Published:
-        <input name="published" value="<?= $_POST["published"] ?? $book["published"] ?>"/>
-    </label>
-    <label>
-        Available:
-        <input name="available" value="<?= $_POST["available"] ?? $book["available"] ?>"/>
-    </label>
-    <input type="hidden" name="id" value="<?= $book["id"]?>"/>
-    <button>Save</button>
-</form>
-
+<article>
+    <div class="edit-box">
+        <div class="bookhub-box">
+            <h1>Edit <?= $book["title"]?></h1>
+        </div>
+        <form method="POST"> 
+            <label>
+                Title:
+                <input class="edit-input" name="title" value="<?= $_POST["title"] ?? $book['title'] ?>"/>
+            </label>
+            <label>
+                Author:
+                <input class="edit-input" name="author" value="<?= $_POST["author"] ?? $book["author"] ?>"/>
+            </label>
+            <label>
+                Published:
+                <input class="edit-input" name="published" value="<?= $_POST["published"] ?? $book["published"] ?>"/>
+            </label>
+            <label>
+                Available:
+                <input class="edit-input" name="available" value="<?= $_POST["available"] ?? $book["available"] ?>"/>
+            </label>
+            <input type="hidden" name="id" value="<?= $book["id"]?>"/>
+            <button class="book-edit-button">Save</button>
+        </form>
+    </div>
+</article>
 
 <?php
     require "views/components/footer.php";
