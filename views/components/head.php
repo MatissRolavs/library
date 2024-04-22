47e84f4917b13198d7e38cbba073cb1970542da1
Matiss Rolavs <matissrolavs2005@gmail.com>
2024-04-22 10:49:33 +0300

    favicon upd

--- views/components/head.php
+++ views/components/head.php
@@ -5,4 +5,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $title ?></title>
     <link rel="stylesheet" href="../style.css">
+    <link rel="icon" type="image/x-icon" href="/images/sins.jpg">
 </head>
